977cd51c9f4b395213a5a516d6d7d9e80e2bfc68
Grey Boolean <greyboolean@gmail.com>
2024-03-04 11:27:17 +0530

    fix exception code bug in prisma-client-exception-filter.ts

--- server/src/pisma-client-exception/pisma-client-exception.filter.ts
+++ server/src/pisma-client-exception/pisma-client-exception.filter.ts
@@ -12,7 +12,7 @@ export class PrismaClientExceptionFilter extends BaseExceptionFilter {
     const message = exception.message.replace(/\n/g, '');
 
     switch (exception.code) {
-      case 'p2002': {
+      case 'P2002': {
         const status = HttpStatus.CONFLICT;
         response.status(status).json({
           statusCode: status,
