7e72f264ebf484dcd030ee82ff89062f0b615829
Grey Boolean <greyboolean@gmail.com>
2024-03-05 05:55:31 +0530

    implement authentication

---
+++ server/src/auth/auth.service.ts
@@ -0,0 +1,42 @@
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthEntity } from './entity/auth.entity';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(username: string, password: string): Promise<AuthEntity> {
+    // Step 1: Fetch a user with the given email
+    const user = await this.prisma.user.findUnique({
+      where: { username: username },
+    });
+
+    // If no user is found, throw an error
+    if (!user) {
+      throw new NotFoundException(`No user found for username: ${username}`);
+    }
+
+    // Step 2: Check if the password is correct
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    // If the password does not match, throw an error
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid password');
+    }
+
+    // Step 3: Generate a JWT containing the usr's ID and return it
+    return {
+      accessToken: this.jwtService.sign({ userId: user.id }),
+    };
+  }
+}
--- server/src/main.ts
+++ server/src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
     .setTitle('Pharma')
     .setDescription('The Pharma API description')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
